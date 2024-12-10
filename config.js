9dde84d0c51bad8546d96c60919a30d0467c06e4
Aymen <102526980+Aymano200@users.noreply.github.com>
2024-12-10 18:42:28 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const config = {
-  challenge: false, // Set to true if you want to enable password protection.
+  challenge: trye, // Set to true if you want to enable password protection.
   blocked: {
     // Normal users can ignore this option. This is for the owner of the website.
-    "gointerstellar.app": "",
+    "standns.live": "",
   },
   users: {
     // You can add multiple users by doing username: 'password'.
